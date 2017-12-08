4137708c036305ed37c6bbdfba12a2892deb4e0a
Keith W. Johnson <hirosh7@gmail.com>
2017-12-07 16:47:38 -0800

    re-did sphinx-quickstart to generate new set of files

--- docs/make.bat
+++ docs/make.bat
@@ -7,9 +7,9 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set SOURCEDIR=source
-set BUILDDIR=build
-set SPHINXPROJ=JenkinsSampleProject
+set SOURCEDIR=.
+set BUILDDIR=_build
+set SPHINXPROJ=ContinuousIntegrationNotes
 
 if "%1" == "" goto help
 
